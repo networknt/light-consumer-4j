cbb07e3f4e15e2169a804f50fb5d3c9a521ab0e8
Nicholas Azar <NicholasAzar@users.noreply.github.com>
2018-08-09 09:54:19 -0400

    Fix race condition over connection caching

--- src/main/java/com/networknt/client/builder/ConnectionCacheManager.java
+++ src/main/java/com/networknt/client/builder/ConnectionCacheManager.java
@@ -26,6 +26,11 @@ public class ConnectionCacheManager {
     /**
      * Entry point to fetch a connection.
      *
+     * Should be synchronized over all instances since if different threads concurrently check if the same connection is
+     * cached, they could both see that it's not available, then attempt to establish 2 connections. The correct flow
+     * in this case would be that one thread would be blocked while checking, while the other thread establishes and
+     * caches the connection. Then the other thread is unblocked, and sees the cached connection available.
+     *
      * @param hostUri The protocol/host/{port} to connect to.
      * @param connectionTTL How long a connection will remain available if a new one needs to be made.
      * @param connectionRequestTimeout How long to wait for a connection to be established.
@@ -33,15 +38,18 @@ public class ConnectionCacheManager {
      * @return A connection to the host.
      */
     public ClientConnection getConnection(URI hostUri, long connectionTTL, TimeoutDef connectionRequestTimeout, boolean isHttp2Enabled) throws InterruptedException, ExecutionException, TimeoutException {
-        CacheableConnection connection = clientConnectionMap.get(hostUri.toString());
-        if (connection != null && connection.isOpen()) {
-            logger.debug("Reusing open connection to: " + hostUri);
-            return connection.getCachedConnection();
+        synchronized (ConnectionCacheManager.class) {
+            CacheableConnection connection = clientConnectionMap.get(hostUri.toString());
+            if (connection != null && connection.isOpen()) {
+                logger.debug("Reusing open connection to: " + hostUri);
+                return connection.getCachedConnection();
+            }
+            logger.debug("Creating a new connection to: " + hostUri);
+            CacheableConnection cacheableConnection = new CacheableConnection(this.connect(hostUri, connectionRequestTimeout, isHttp2Enabled), connectionTTL);
+
+            clientConnectionMap.put(hostUri.toString(), cacheableConnection);
+            return cacheableConnection.getCachedConnection();
         }
-        logger.debug("Creating a new connection to: " + hostUri);
-        CacheableConnection cacheableConnection = new CacheableConnection(this.connect(hostUri, connectionRequestTimeout, isHttp2Enabled), connectionTTL);
-        clientConnectionMap.put(hostUri.toString(), cacheableConnection);
-        return cacheableConnection.getCachedConnection();
     }
 
     /**
