6dce8b7957e0dae6f803e30bba8a267c3e872cb1
sreenivolvo <36326564+sreenivolvo@users.noreply.github.com>
2019-02-27 20:42:37 +0530

    updated test cases

--- src/test/java/com/networknt/client/builder/TestHttpClientBuilder.java
+++ src/test/java/com/networknt/client/builder/TestHttpClientBuilder.java
@@ -8,16 +8,15 @@ import com.networknt.service.SingletonServiceFactory;
 import io.undertow.client.ClientConnection;
 import io.undertow.client.ClientRequest;
 import io.undertow.util.Headers;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
@@ -32,13 +31,9 @@ import static org.mockito.Mockito.*;
 public class TestHttpClientBuilder {
 
     @Mock
-    Cluster cluster;
-    @Mock
     ConsumerConfig consumerConfig;
     @Mock
     Config config;
-    @Mock
-    Http2Client http2Client;
     @Mock
     ConnectionCacheManager connectionCacheManager;
     @Mock
@@ -48,20 +43,26 @@ public class TestHttpClientBuilder {
     @Mock
     ServiceDef serviceDef;
 
+    static Cluster cluster = Mockito.mock(Cluster.class);
+    static Http2Client http2Client = Mockito.mock(Http2Client.class);
+
+    URI uri;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp1() throws Exception {
         PowerMockito.mockStatic(SingletonServiceFactory.class);
         PowerMockito.when(SingletonServiceFactory.getBean(Cluster.class)).thenReturn(cluster);
-        PowerMockito.mockStatic(Http2Client.class);
-        PowerMockito.when(Http2Client.getInstance()).thenReturn(http2Client);
         PowerMockito.whenNew(ConnectionCacheManager.class).withNoArguments().thenReturn(connectionCacheManager);
         PowerMockito.whenNew(HttpClientRequest.class).withNoArguments().thenReturn(httpClientRequest);
+
+        PowerMockito.mockStatic(Http2Client.class);
+        PowerMockito.when(Http2Client.getInstance()).thenReturn(http2Client);
 
         PowerMockito.when(httpClientRequest.getConnectionCacheTTLms()).thenReturn(10000l);
         PowerMockito.when(httpClientRequest.getHttp2Enabled()).thenReturn(true);
         PowerMockito.when(httpClientRequest.getMaxReqCount()).thenReturn(-1);
         when(httpClientRequest.getApiHost()).thenReturn("https://localhost:8080");
-        URI uri = new URI("https://localhost:8080");
+        uri = new URI("https://localhost:8080");
         when(connectionCacheManager.getConnection(uri, 10000, null, true, -1)).thenReturn(clientConnection);
     }
 
@@ -73,8 +74,8 @@ public class TestHttpClientBuilder {
         PowerMockito.when(httpClientRequest.getAuthToken()).thenReturn("12345abc");
         HttpClientBuilder cb = new HttpClientBuilder();
         cb.send();
-        verify(http2Client, times(1)).addAuthToken(any(), eq("12345abc"));
-        verify(http2Client, never()).addCcToken(any());
+        verify(http2Client, times(1)).addAuthToken(anyObject(), anyString());
+        verify(http2Client, never()).addCcToken(clientRequest);
         Assert.assertEquals("12345abc", cb.getAuthToken());
     }
 
@@ -86,74 +87,55 @@ public class TestHttpClientBuilder {
         PowerMockito.when(httpClientRequest.getAuthToken()).thenReturn(null);
         HttpClientBuilder cb = new HttpClientBuilder();
         cb.send();
-        verify(http2Client, never()).addAuthToken(any(), eq("12345abc"));
+        verify(http2Client, never()).addAuthToken(any(), eq("321321"));
         verify(http2Client, times(1)).addCcToken(any());
     }
 
     @Test
-    public void testHttpClientBuilderHost() throws Exception {
+    public void testHttpClientBuilderApiHost() throws Exception {
         ClientRequest clientRequest = new ClientRequest();
         clientRequest.getRequestHeaders().put(Headers.HOST, "localhost");
         PowerMockito.when(httpClientRequest.getClientRequest()).thenReturn(clientRequest);
         HttpClientBuilder cb = new HttpClientBuilder();
         cb.send();
-        verify(httpClientRequest, times(4)).getClientRequest();
-        Assert.assertEquals("localhost", httpClientRequest.getClientRequest().getRequestHeaders().get(Headers.HOST).getFirst());
+        verify(httpClientRequest, times(1)).getApiHost();
+        Assert.assertEquals("https://localhost:8080", httpClientRequest.getApiHost());
     }
 
     @Test
-    public void testHttpClientBuilderWithoutHost() throws Exception {
+    public void testHttpClientBuilderHost() throws Exception {
         ClientRequest clientRequest = new ClientRequest();
         PowerMockito.when(httpClientRequest.getClientRequest()).thenReturn(clientRequest);
         HttpClientBuilder cb = new HttpClientBuilder();
         cb.send();
-        verify(httpClientRequest, times(3)).getClientRequest();
+        Assert.assertEquals("localhost", uri.getHost());
     }
 
     @Test
-    public void testHttpClientBuilderWithoutEnvConfig() throws Exception {
-        PowerMockito.when(serviceDef.getEnvironment()).thenReturn("dev");
-        PowerMockito.when(cluster.serviceToUrl(anyString(), anyString(), anyString(), anyString())).thenReturn("https://localhost:8080");
-        PowerMockito.when(httpClientRequest.getServiceDef()).thenReturn(serviceDef);
-        HttpClientBuilder cb = new HttpClientBuilder();
-        cb.setServiceDef(serviceDef);
-        verify(serviceDef, times(2)).getEnvironment();
-        verify(httpClientRequest, times(4)).getServiceDef();
-    }
-
-    @Test
-    public void testHttpClientBuilderWithEnvConfig() throws Exception {
+    public void testHttpClientBuilderEnvConfig() throws Exception {
         Map<String, String> map = new HashMap<String, String>();
-        map.put("com.networknt.hello-1", "dev1");
+        map.put("com.networknt.hello-3", "dev2");
         when(consumerConfig.getServiceEnv()).thenReturn(map);
         PowerMockito.mockStatic(Config.class);
         PowerMockito.when(Config.getInstance()).thenReturn(config);
         PowerMockito.when(config.getJsonObjectConfig("consumer", ConsumerConfig.class)).thenReturn(consumerConfig);
-
-        PowerMockito.when(serviceDef.getServiceId()).thenReturn("com.networknt.hello-1");
-        PowerMockito.when(cluster.serviceToUrl(anyString(), anyString(), anyString(), anyString())).thenReturn("https://localhost:8080");
+        PowerMockito.when(serviceDef.getServiceId()).thenReturn("com.networknt.hello-3");
         PowerMockito.when(httpClientRequest.getServiceDef()).thenReturn(serviceDef);
         HttpClientBuilder cb = new HttpClientBuilder();
         cb.setServiceDef(serviceDef);
-        verify(serviceDef, times(2)).getEnvironment();
+        Assert.assertEquals("dev2", consumerConfig.getServiceEnv().get(serviceDef.getServiceId()));
+
     }
 
     @Test
     public void testHttpClientBuilderServiceUrl() throws Exception {
         PowerMockito.when(httpClientRequest.getServiceDef()).thenReturn(serviceDef);
-        PowerMockito.when(serviceDef.getProtocol()).thenReturn("https");
-        PowerMockito.when(serviceDef.getServiceId()).thenReturn("com.networknt.hello-1");
         PowerMockito.when(serviceDef.getEnvironment()).thenReturn("dev");
-        PowerMockito.when(serviceDef.getRequestKey()).thenReturn("dev");
-        PowerMockito.when(cluster.serviceToUrl("https", "com.networknt.hello-1", "dev", "dev")).thenReturn("https://localhost:8080");
-
+        PowerMockito.when(cluster.serviceToUrl(any(), any(), any(), any())).thenReturn("https://localhost:8888");
         HttpClientBuilder cb = new HttpClientBuilder();
         cb.setServiceDef(serviceDef);
-        verify(serviceDef, times(2)).getEnvironment();
-        verify(httpClientRequest, times(4)).getServiceDef();
-
+        Assert.assertEquals("https://localhost:8888", cluster.serviceToUrl(any(), any(), any(), any()));
     }
-
 }
 
 
